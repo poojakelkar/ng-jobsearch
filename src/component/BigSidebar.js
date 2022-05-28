4eb1a9cc2e4484dda38a4c9393e97336d627882a
Pooja Kelkar <poojakelkar53@gmail.com>
2022-05-28 22:47:35 +0530

    feature: added dashborad

--- src/component/BigSidebar.js
+++ src/component/BigSidebar.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { Logo } from "./Logo";
+import NavbarLinks from "./NavbarLinks";
 
 export const BigSidebar = () => {
+    const { isSidebarOpen } = useSelector((store) => store.user);
     return (
         <Wrapper>
-            <h1>BigSidebar</h1>
+            <div
+                className={
+                    isSidebarOpen
+                        ? "sidebar-container show-hide"
+                        : "sidebar-container"
+                }>
+                <div className='content'>
+                    <header>
+                        <Logo />
+                    </header>
+                    <NavbarLinks />
+                </div>
+            </div>
         </Wrapper>
     );
 };
