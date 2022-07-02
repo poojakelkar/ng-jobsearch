8055c165af898c8819cf97c6d31d2c820bf69244
Pooja Kelkar <poojakelkar53@gmail.com>
2022-07-02 21:03:12 +0530

    fix: refactor project

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,9 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import customFetch from "../../utils/axios";
-import { logoutuser } from "../user/userSlice";
-import { getUser } from "../../utils/localStorage";
-import { showLoading, hideLoading, getAllJobs } from "../allJobs/allJobsSlice";
+import { createJobThunk, deleteJobThunk, editJobThunk } from "./jobThunk";
 
 const initialState = {
     isLoading: false,
@@ -18,69 +15,9 @@ const initialState = {
     editJobId: "",
 };
 
-export const createJob = createAsyncThunk(
-    "job/createJob",
-    async (job, thunkAPI) => {
-        try {
-            console.log(getUser());
-            const resp = await customFetch.post("/jobs", job, {
-                headers: {
-                    authorization: `Bearer ${
-                        thunkAPI.getState().user.user.payload.user.token
-                    }`,
-                },
-            });
-            thunkAPI.dispatch(clearValues());
-            return resp.data;
-        } catch (error) {
-            if (error.response.status === 401) {
-                thunkAPI.dispatch(logoutuser());
-                thunkAPI.rejectWithValue("Unauthorized Access...Logging Out");
-            }
-            return thunkAPI.rejectWithValue(error.response.data.msg);
-        }
-    }
-);
-export const deleteJob = createAsyncThunk(
-    "job/deleteJob",
-    async (jobId, thunkAPI) => {
-        thunkAPI.dispatch(showLoading());
-        try {
-            console.log(jobId);
-            const resp = await customFetch.delete(`/jobs/${jobId}`, {
-                headers: {
-                    authorization: `Bearer ${
-                        thunkAPI.getState().user.user.payload.user.token
-                    }`,
-                },
-            });
-            thunkAPI.dispatch(getAllJobs());
-            return resp.data.msg;
-        } catch (error) {
-            thunkAPI.dispatch(hideLoading());
-            return thunkAPI.rejectWithValue(error.response.data.msg);
-        }
-    }
-);
-
-export const editJob = createAsyncThunk(
-    "job/editJob",
-    async ({ jobId, job }, thunkAPI) => {
-        try {
-            const resp = await customFetch.patch(`/jobs/${jobId}`, job, {
-                headers: {
-                    authorization: `Bearer ${
-                        thunkAPI.getState().user.user.payload.user.token
-                    }`,
-                },
-            });
-            thunkAPI.dispatch(clearValues());
-            return resp.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.response.data.msg);
-        }
-    }
-);
+export const createJob = createAsyncThunk("job/createJob", createJobThunk);
+export const deleteJob = createAsyncThunk("job/deleteJob", deleteJobThunk);
+export const editJob = createAsyncThunk("job/editJob", editJobThunk);
 
 const jobSlice = createSlice({
     name: "job",
---
+++ src/features/job/jobThunk.js
@@ -0,0 +1,59 @@
+import { showLoading, hideLoading, getAllJobs } from "../allJobs/allJobsSlice";
+import customFetch from "../../utils/axios";
+import { clearValues } from "./jobSlice";
+import { getUser } from "../../utils/localStorage";
+import { logoutuser } from "../user/userSlice";
+
+export const createJobThunk = async (job, thunkAPI) => {
+    try {
+        console.log(getUser());
+        const resp = await customFetch.post("/jobs", job, {
+            headers: {
+                authorization: `Bearer ${
+                    thunkAPI.getState().user.user.payload.user.token
+                }`,
+            },
+        });
+        thunkAPI.dispatch(clearValues());
+        return resp.data;
+    } catch (error) {
+        if (error.response.status === 401) {
+            thunkAPI.dispatch(logoutuser());
+            thunkAPI.rejectWithValue("Unauthorized Access...Logging Out");
+        }
+        return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+};
+export const deleteJobThunk = async (jobId, thunkAPI) => {
+    thunkAPI.dispatch(showLoading());
+    try {
+        console.log(jobId);
+        const resp = await customFetch.delete(`/jobs/${jobId}`, {
+            headers: {
+                authorization: `Bearer ${
+                    thunkAPI.getState().user.user.payload.user.token
+                }`,
+            },
+        });
+        thunkAPI.dispatch(getAllJobs());
+        return resp.data.msg;
+    } catch (error) {
+        thunkAPI.dispatch(hideLoading());
+        return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+};
+export const editJobThunk = async ({ jobId, job }, thunkAPI) => {
+    try {
+        const resp = await customFetch.patch(`/jobs/${jobId}`, job, {
+            headers: {
+                authorization: `Bearer ${
+                    thunkAPI.getState().user.user.payload.user.token
+                }`,
+            },
+        });
+        thunkAPI.dispatch(clearValues());
+        return resp.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+};
