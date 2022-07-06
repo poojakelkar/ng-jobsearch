04049d1d6283aa85d5978ee0d9384171026e2a66
Pooja Kelkar <poojakelkar53@gmail.com>
2022-07-06 22:02:53 +0530

    feature: added stats

--- src/component/ChartsContainer.js
+++ src/component/ChartsContainer.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ChartsContainer = () => {
+    const [barChart, setBarChart] = useState(true);
+
     return <h1>ChartsContainer</h1>;
 };
 
