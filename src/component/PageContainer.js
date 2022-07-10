fe3ed2a9be6fba3522cbb73ca52472d59efc7550
Pooja Kelkar <poojakelkar53@gmail.com>
2022-07-10 14:37:03 +0530

    feature: added pagination buttons

--- src/component/PageContainer.js
+++ src/component/PageContainer.js
@@ -1,7 +1,43 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
 
 const PageContainer = () => {
-    return <h2>PageContainer</h2>;
+    const { numOfPages, page } = useSelector((store) => store.allJobs);
+    const dispatch = useDispatch();
+
+    const pages = Array.from({ length: numOfPages }, (_, index) => {
+        return index + 1;
+    });
+    const nextPage = () => {
+        console.log("next page");
+    };
+    const prevPage = () => {
+        console.log("prev page");
+    };
+    return (
+        <Wrapper>
+            <button className='prev-btn' onClick={prevPage}>
+                <HiChevronDoubleLeft />
+                Prev
+            </button>
+            {pages.map((pageNumber) => {
+                return (
+                    <button
+                        type='button'
+                        key={pageNumber}
+                        onClick={() => console.log("changed Page")}>
+                        {pageNumber}
+                    </button>
+                );
+            })}
+            <button className='prev-btn' onClick={nextPage}>
+                <HiChevronDoubleRight />
+                Next
+            </button>
+        </Wrapper>
+    );
 };
 
 export default PageContainer;
