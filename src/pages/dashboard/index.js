50161fe3e71f4f4c16f930ccba98fd163c2c402f
Pooja Kelkar <poojakelkar53@gmail.com>
2022-07-14 21:44:47 +0530

    empty the repo

--- src/pages/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-import { AddJobs } from "./AddJobs";
-import { AllJobs } from "./AllJobs";
-import { Profile } from "./Profile";
-import { SharedLayout } from "./SharedLayout";
-import { Stats } from "./Stats";
-
-export { Profile, AddJobs, AllJobs, Stats, SharedLayout };
