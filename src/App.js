da08bac26d595766731dffc82ab939e3bbfe7aa4
Pooja Kelkar <poojakelkar53@gmail.com>
2022-07-14 22:18:57 +0530

    Feature: Added folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { LandingPage, Error, Register, ProtectedRoute } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+    Profile,
+    AddJobs,
+    Stats,
+    AllJobs,
+    SharedLayout,
+} from "./pages/dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <ProtectedRoute>
+                            <SharedLayout />
+                        </ProtectedRoute>
+                    }>
+                    <Route index element={<Stats />} />
+                    <Route path='all-jobs' element={<AllJobs />} />
+                    <Route path='add-jobs' element={<AddJobs />} />
+                    <Route path='profile' element={<Profile />} />
+                </Route>
+                <Route path='landing' element={<LandingPage />}></Route>
+                <Route path='register' element={<Register />}></Route>
+                <Route path='*' element={<Error />}></Route>
+            </Routes>
+            <ToastContainer position='top-center' />
+        </BrowserRouter>
+    );
 }
 
 export default App;
