bcc177214682e811da9846c72fd4b12d0c7f8ea4
Pooja Kelkar <poojakelkar53@gmail.com>
2022-07-14 22:58:19 +0530

    feat: added small sidebar and bigsidebar

---
+++ src/features/user/userThunk.js
@@ -0,0 +1,52 @@
+import { clear } from "@testing-library/user-event/dist/clear";
+import { toast } from "react-toastify";
+import customFetch, { checkForUnauthorizeResponse } from "../../utils/axios";
+import { clearValues } from "../job/jobSlice";
+import { logoutuser } from "./userSlice";
+
+export const registerUserThunk = async (url, user, thunkAPI) => {
+    try {
+        const resp = customFetch.post(url, user);
+        return resp.data;
+    } catch (error) {
+        toast.error(error.response.data.msg);
+        return checkForUnauthorizeResponse(error, thunkAPI);
+    }
+};
+
+export const loginUserThunk = async (url, user, thunkAPI) => {
+    try {
+        const resp = await customFetch.post(url, user);
+        return resp.data;
+    } catch (error) {
+        toast.error(error.response.data.msg);
+        return checkForUnauthorizeResponse(error, thunkAPI);
+    }
+};
+
+export const updateUserThunk = async (url, user, thunkAPI) => {
+    try {
+        console.log(thunkAPI.getState());
+        const resp = await customFetch.patch(url, user);
+        return resp.data;
+    } catch (error) {
+        if (error.response.status === 401) {
+            thunkAPI.dispatch(logoutuser());
+            return thunkAPI.rejectWithValue(
+                "Unauthorized User Access..Logging Out"
+            );
+        }
+        return checkForUnauthorizeResponse(error, thunkAPI);
+    }
+};
+
+export const clearStoreThunk = async (message, thunkAPI) => {
+    try {
+        thunkAPI.dispatch(logoutuser(message));
+        thunkAPI.dispatch(clear);
+        thunkAPI.dispatch(clearValues());
+        return Promise.resolve();
+    } catch (error) {
+        return Promise.reject();
+    }
+};
